7423220dc13aad5aa48b9e1ad7659bfff37418b9
helldotrs <macrosystem@proton.me>
2024-03-10 15:26:55 +0100

    creating convert-list

---
+++ unicorne-to-ez-converter/convert-list.part.py
@@ -0,0 +1,13 @@
+layout = [
+
+    [f1,f2,f3,f4,f5,f6, e1,e2, f7,f8,f9,f10,f11,f12], #ergodox
+
+    [a1,a2,a3,a4,a5,a6, e1,e2, a7,a8,a9,a10,a11,a12], #e - ergodox
+    [a1,a2,a3,a4,a5,a6, e1,e2, a7,a8,a9,a10,a11,a12], #e - ergodox
+    [a1,a2,a3,a4,a5,a6,        a7,a8,a9,a10,a11,a12], #e - ergodox
+
+    cluster = "blank",
+
+    [e1,e2,e3,e4,   c1, c2,c3,c4,c5, c6,    e5,e6,e7,e8]
+]
+
