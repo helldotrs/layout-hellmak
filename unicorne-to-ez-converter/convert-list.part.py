560d4a8763867f635982a15917eb787668efa05e
helldotrs <macrosystem@proton.me>
2024-03-10 15:34:05 +0100

    more logicail convert-list

--- unicorne-to-ez-converter/convert-list.part.py
+++ unicorne-to-ez-converter/convert-list.part.py
@@ -1,13 +1,15 @@
-layout = [
-
-    [f1,f2,f3,f4,f5,f6, e1,e2, f7,f8,f9,f10,f11,f12], #ergodox
-
+keys_function_row = [ #ergodox only
+    [f1,f2,f3,f4,f5,f6, e1,e2, f7,f8,f9,f10,f11,f12],
+]
+keys_alpha_rows = [
     [a1,a2,a3,a4,a5,a6, e1,e2, a7,a8,a9,a10,a11,a12], #e - ergodox
     [a1,a2,a3,a4,a5,a6, e1,e2, a7,a8,a9,a10,a11,a12], #e - ergodox
     [a1,a2,a3,a4,a5,a6,        a7,a8,a9,a10,a11,a12], #e - ergodox
-
-    cluster = "blank",
-
-    [e1,e2,e3,e4,   c1, c2,c3,c4,c5, c6,    e5,e6,e7,e8]
+]
+keys_cluster = [ #ergodox only
+ #all blank by default
+]
+keys_control_row = [ #ergodox only, c2,c3,c4,c5 overwrites the 2 * 2 * u2 keys in the cluster
+    [e1,e2,e3,e4,   c1, c2,c3,c4,c5, c6,    e5,e6,e7,e8] #e ergodox
 ]
 
